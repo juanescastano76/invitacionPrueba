2973698caf6ebec3f65fb1f25c10c7b18d91fc57
JuanCastano <juanescastano76@gmail.com>
2025-09-22 00:38:35 -0500

    fixed type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
     }
   };
 
-  const audioRef = useRef(null);
+  const audioRef = useRef<HTMLAudioElement | null>(null);
 
   useEffect(() => {
     if (audioRef.current) {
